60f283a764fd8d6738083126364ce7310811ecdc
cyrus david pastelero <cgpastelero@up.edu.ph>
2020-08-02 16:00:22 +0800

    updated max_pairwise

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -3,19 +3,22 @@
 #include <algorithm>
 
 long long MaxPairwiseProduct(const std::vector<int>& numbers) {
-    long long int first, second;
+    long long int first = numbers[0], second = numbers[1];
     int n = numbers.size();
     long long int maxPair;
 
+    if (second > first)
+	   std::swap(second,first);
+
     //First, set first to be the biggest number between index 1 and 0
-    if (numbers[0] > numbers[1]) {
+/*    if (numbers[0] > numbers[1]) {
         first = numbers[0];
         second = numbers[1];
     } else {
         first = numbers[1];
         second = numbers[0];
     }
-
+*/
     //compare the each numbers in the vector one by one with first
     //if the number is less than first, compare it to second, move on
     for (int i = 2; i < n; i++) {
